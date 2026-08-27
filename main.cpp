902e853f70adb3b10abd519257be3c431c82a5d5
Sorya Dawood <soryadawood@Soryas-MacBook.local>
2026-08-27 10:35:53 -0700

    Complete 1-100 loop

--- main.cpp
+++ main.cpp
@@ -2,9 +2,6 @@
 using namespace std;
 
 int main() {
-    // complete this code in order to make it output
-    // the numbers 1-100
-
     for (int i = 1; i <= 100; i++)
         cout << i << " ";
 
