c31fb94773af3c05e6802dc677cc2bddf20610cf
SHaidari262 <shaidari262@insite.4cd.edu>
2026-08-26 23:11:36 -0700

    Output numbers from 1 to 100 in main.cpp
    Modified main.cpp to output numbers from 1 to 100.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    // complete this code in order to make it output
+    // the numbers 1-100
+
+    for (int i = 1; i <= 100; i++)
+        cout << i << " ";
+
     return 0;
 }
